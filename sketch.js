9fb992d8cdfdfcab97089cb6f2b0587472611aef
jgarc238 <78009862+jgarc238@users.noreply.github.com>
2021-09-08 00:29:54 -0500

    This is my 1st process of playing with P5.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,61 @@
 function setup() {
-    createCanvas(600, 400);
-    
+    createCanvas(windowWidth, windowHeight);
+    bubble = new Bubble(300, 400);
+    bubble2 = new Bubble(500, 20);
+    bubble3 = new Bubble(1500, 500);
 }
 
 function draw() {
     background(0);
+    bubble.move();
+    bubble.show();
+
+    bubble2.move();
+    bubble2.show();
+
+    bubble3.move();
+    bubble3.show();
+
+    var x = 200;
+	var y = 400;
+	var e = ellipse(pmouseX, pmouseY, x, y);
+
+	if (key == 'r'){
+		e = rect(pmouseX, pmouseY, x, y);
+		stroke('red');
+		strokeWeight(10)
+		fill('green');
+	}
+
+
+	if (key == 't'){
+		strokeWeight(5)
+		stroke('blue');
+		fill('orange');
+		
+	}
+	if (key == 'q'){
+		background("blue");
+}
     
+}
+
+class Bubble {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+
+    move(){
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+    }
+
+    show(){
+        
+        strokeWeight(4);
+        fill(255);
+        ellipse(this.x, this.y - 100, 200);
+        
+    }
 }
