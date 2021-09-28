e45a7d4081465923ad3f20d265822c8ca6f01952
jgarc238 <78009862+jgarc238@users.noreply.github.com>
2021-09-28 09:38:15 -0500

    final changes

--- sketch.js
+++ sketch.js
@@ -2,10 +2,10 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   balls = [];
   squares = [];
-  arr = [1000];
+  arr = [windowWidth];
   noCursor();
   background(220);
-  alert("The Color Coder by Jonathan Garcia \n My focus for this project is color and interaction.\n You can draw with the cursor, and the computer can also draw with \n lines.\n Left-click = ball trail\n r = rectangle/reset\n l = lines\n c + Left-click = single ball + background change");
+  alert("The Color Coder by Jonathan Garcia \n My focus for this project is color and interaction.\n You can draw with the cursor, and the computer can also draw with \n lines.\n Left-click = ball trail\n r = rectangle/green eraser\n l = lines\n w + Left-click = single ball + background white\n b+ Left-click = single ball + background black");
   
 
 }
@@ -13,7 +13,7 @@ function setup() {
 function draw() {
   
   noStroke();
-  fill(random(0, 255), random(0, 255), random(0, 255));
+  fill("red");
   var x = 30;
   var y = 30;
   var e = ellipse(pmouseX, pmouseY, x, y);
@@ -21,18 +21,23 @@ function draw() {
   stroke(0);
   
   if (key == "l") {
-    stroke(random(0, 255), random(0, 255), random(0, 255));
+    stroke("blue");
     for(let i = 0; i < 10; i++){
-      line(random(arr[0], arr[10000]), random(arr[0], arr[10000]), random(arr[0], arr[10000]), random(arr[0], arr[10000]));
+      line(random(arr[0], arr[windowWidth]), random(arr[0], arr[windowHeight]), random(arr[0], arr[windowWidth]), random(arr[0], arr[windowHeight]));
     }
   }
   
-  if(key == "c"){
+  if(key == "w"){
     
-    background(0);
+    background(255);
     
   }
-  
+
+  if(key == "b"){
+
+    background(0);
+
+  }
 
   for (let i = 0; i < balls.length; i++) {
     balls[i].update();
@@ -40,11 +45,13 @@ function draw() {
   }
 
   if (key == "r") {
-    background(220);
+    
     squares.push(new Square(mouseX, mouseY));
     for (let i = 0; i < squares.length; i++) {
       squares[i].show();
+      squares[i].update();
     }
+    
   }
   
   if(squares.length > 10){
@@ -76,7 +83,7 @@ function Ball(x, y) {
   };
   this.show = function () {
     noStroke();
-    fill(random(0, 255), random(0, 255), random(0, 255));
+    fill("red");
     ellipse(this.pos.x, this.pos.y, 30, 30);
   };
 }
@@ -84,10 +91,26 @@ function Ball(x, y) {
 
 function Square(x, y) {
   this.pos = createVector(x, y);
+  
+
+  this.dir = createVector(random(-1, 1), random(-1, 1));
+  this.speed = (10, 10);
+
+  this.update = function () {
+    uPos = p5.Vector.mult(this.dir, this.speed);
+    this.pos.add(uPos);
+
+    if (this.pos.x < 0 && this.pos.x > windowWidth) {
+      this.dir.x *= -1;
+    }
+    if (this.pos.y < 0 && this.pos.y > windowHeight) {
+      this.dir.y *= -1;
+    }
+  };
 
   this.show = function () {
-    stroke(0);
-    fill(0);
+    noStroke();
+    fill(0, 255, 0);
     rect(this.pos.x - 15, this.pos.y - 15, 30, 50);
   };
 }
